bc06f6df8605d94e3546bc19b7e34f50ad2350b4
quint <quint@137.125.150.78>
2021-09-21 14:43:59 -0400

    The use of static on variables and methods

---
+++ src/main/java/com/mycompany/week4_demo1/NewClass.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.week4_demo1;
+
+
+public class NewClass {
+    int age = 10;
+    
+    public static void main(String[] args) {
+        Helper h1 = new Helper();
+        Helper h2 = new Helper();
+        Helper h3 = new Helper();
+        h1.age= 5;
+        h2.age= 5;
+        h3.age= 5;
+        
+        System.out.println("The age is "  + h1.getAge());
+        System.out.println("The age is "  + h2.getAge());
+        System.out.println("The age is "  + h3.getAge());
+        
+    }
+    
+}
+
+class Helper {
+    public int age;
+    public static int cube(int side) {return side*side * side;}
+    public int getAge(){return age;}
+}
